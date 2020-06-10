ee9e6918f884d59fe3a24e7ceebdbe93057ad49d
Evgeny Minikh <evgeny.minih@gmail.com>
2020-06-10 14:29:22 +0300

    [Add] Added new script to add new VLAN to all vSwithes in datacenter

---
+++ Add_new_VLAN_to_all_hosts_in_DC.ps1
@@ -0,0 +1,23 @@
+﻿function Add-NewVLANtoDatacenter {
+    param (
+        [Parameter(Mandatory=$true)] $VLAN_Name,
+        [Parameter(Mandatory=$true)] $VLAN_ID,
+        [Parameter(Mandatory=$true)] $vSwitchName,
+        [Parameter(Mandatory=$true)] $DataCenter
+    )
+    Get-Datacenter -Name $DataCenter | Get-VMHost | Get-VirtualSwitch -name $vSwitchName | New-VirtualPortGroup -Name $VLAN_Name -VLanId $VLAN_ID
+}
+
+$cred = Get-Credential
+$vCenter = '#vCenter Name to connect to'
+$vSwitchName = '#VSwitch name where to add a new VLAN'
+$datacenter = '#Datacenter Name'
+
+$newVLANID = #VLAN Number
+$newVLANName = '#New VLAN Name'
+
+Connect-VIServer -Server $vCenter -Credential $cred
+
+Add-NewVLANtoDatacenter -VLAN_Name $newVLANName -VLAN_ID $newVLANID -vSwitchName $vSwitchName -DataCenter $datacenter
+
+Disconnect-VIServer -Confirm:$false
